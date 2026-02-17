fd99a6005ce0d22ac83ec77b483859cb46055627
Juan Alves <juangalvesx@gmail.com>
2026-02-17 15:17:41 -0300

    Criado a controller

--- src/main/java/com/github/marcellgalves/cadastroapi/CadastroSpringApplication.java
+++ src/main/java/com/github/marcellgalves/cadastroapi/CadastroSpringApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroSpringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CadastroSpringApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(CadastroSpringApplication.class, args);}
 
 }
